c7f497625cbc671d78e1bc241e8f16e2ca6d2739
Nate Shoemaker <nate@hintmedia.com>
2017-09-29 06:41:01 -0700

    Init

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "extends": ["standard", "standard-react"],
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "space-before-function-paren": 0,
+    "react/prop-types": 0,
+    "react/jsx-no-bind": 0,
+    "no-new": 0,
+    "object-property-newline": 0,
+    "object-curly-spacing": ["error", "always"],
+    "eol-last": ["error", "always"],
+    "arrow-parens": ["error", "as-needed"],
+    "indent": 0,
+    "no-return-assign": 0,
+    "brace-style": 0
+  }
+}
